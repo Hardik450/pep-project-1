b67aef92658879595eb686b845e5366c635e7cf1
Hardik Jain Harsora <71239504+Hardik450@users.noreply.github.com>
2025-07-19 10:19:05 +0530

    Create task_3.py

---
+++ task_3.py
@@ -0,0 +1,10 @@
+# Task 3: String Manipulation
+
+# Define the string
+text = "Hello World"
+
+# Extract the first three characters
+substring = text[:3]
+
+# Print the extracted substring
+print("Extracted substring:", substring)
